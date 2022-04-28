10dbd6f2380284931f8d542c4e6dfbbc13f34748
Polina Kalinina <kalinina_polinka@mail.ru>
2022-04-28 20:53:59 +0400

    fixes Matrix

--- src/Matrix.java
+++ src/Matrix.java
@@ -6,28 +6,27 @@ public class Matrix {
         row = n;
         column = m;
         matrix = new int[row][column];
-
-        //Заполнение и печать матрицы
+        //Заполнение матрицы
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < column; j++) {
                 matrix[i][j] = (int) (Math.random() * 100);
-                System.out.print("\t" + matrix[i][j]);
             }
-            System.out.println();
         }
-        System.out.println();
     }
 
     public Matrix(int[][] arr) {
-        this.matrix = arr;//В целом, так не очень хорошо делать, так как массивы являются ссылочными типами и
-        //если изменить arr где-то за пределами, то изменится и matrix, что приведет к неожиданным проблемам.
-        //Лучше выделит новую памяь и поэлементно скопировать
-        row = arr.length;
-        column = arr.length;
-        // Вывод матрицы лучше в отдлеьный метод,так как код дублируется здесь и в предыдущем конструкторе. 
-        //Плюс у вас будет возможность вывести когда хотите. Это Нарушение принипа Единственной ответсвенности, который можете загуглить в интернете
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
+                row = arr.length;
+                column = arr[i].length;
+            }
+        }
+        matrix = arr.clone();
+    }
+
+    public void print() {
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
                 System.out.print("\t" + matrix[i][j]);
             }
             System.out.println();
@@ -37,22 +36,26 @@ public class Matrix {
 
     public Matrix add(Matrix matrix2) {
         int[][] sum = new int[row][column];
+        int[][] temp = new int[0][0];
         if (matrix2.row == row && matrix2.column == column) {
             System.out.println("Сумма матриц :");
             for (int i = 0; i < row; i++) {
                 for (int j = 0; j < column; j++) {
                     sum[i][j] = matrix[i][j] + matrix2.matrix[i][j];
+                    System.out.print("\t" + sum[i][j]);
                 }
+                System.out.println();
             }
+            return new Matrix(sum);
         } else
             System.out.println("Cкладывать матрицы можно только одинаковой размерности!");
-        return new Matrix(sum);//Должны быть разные return для разных условий
+        return new Matrix(temp);
     }
 
     public Matrix multi(Matrix matrix2) {
         int[][] multi = new int[row][column];
+        int[][] temp = new int[0][0];
         if (matrix2.row == row && matrix2.column == column) {
-            System.out.println("Произведение матриц :");
             for (int i = 0; i < row; i++) {
                 for (int j = 0; j < column; j++) {
                     for (int k = 0; k < column; k++) {
@@ -60,10 +63,19 @@ public class Matrix {
                     }
                 }
             }
+            System.out.println("Произведение матриц :");
+            for (int i = 0; i < row; i++) {
+                for (int j = 0; j < column; j++) {
+                    System.out.print("\t" + multi[i][j]);
+                }
+                System.out.println();
+            }
+            return new Matrix(multi);
         } else
             System.out.println("Умножать матрицы можно только одинаковой размерности!");
-        return new Matrix(multi);//Должны быть разные return для разных условий
+        return new Matrix(temp);
     }
+
 
     public void transposed() {
         System.out.println("Транспонированная матрица: ");
@@ -84,15 +96,18 @@ public class Matrix {
 
     public static void main(String args[]) {
         Matrix m1 = new Matrix(3, 3);
-        Matrix m2 = new Matrix(3, 3);
+        m1.print();
+
         int[][] arr = {{1, 2, 3}, {1, 3, 6}, {3, 6, 7}, {1, 2, 3}};
         Matrix m4 = new Matrix(arr);
+
         int[][] arr1 = {{1, 2, 3}, {1, 3, 6}, {3, 6, 7}};
         Matrix m5 = new Matrix(arr1);
+        m5.print();
 
-        m1.add(m4);
-        m1.multi(m4);
-        m1.add(m5);
         m1.multi(m5);
+//        m1.add(m5);
+//        m1.add(m4);
+//        m1.multi(m4);
     }
 }
